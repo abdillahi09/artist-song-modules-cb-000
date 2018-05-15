99410c65b6f2ed5e17ea2779882c35aa5b3e6863
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-15 22:15:42 +0000

    automatically backed up by learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -8,4 +8,15 @@ module memorable
       def count
         self.all.count
       end
+
+    end
+
+  module InstsanceMethods
+
+    def initialize
+      self.class.all << self
+    end
+
+  end
+
 end
