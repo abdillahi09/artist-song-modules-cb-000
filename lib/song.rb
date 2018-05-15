70fc94316108dd1ab3613173aef556121ea46a77
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-15 21:34:12 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,9 +10,9 @@ class Song
     @@songs << self
   end
 
-  def self.all
-    @@songs
-  end
+  # def self.all
+  #   @@songs
+  # end
 
   def self.find_by_name(name)
     @@songs.detect{|a| a.name == name}
@@ -22,9 +22,9 @@ class Song
     @artist = artist
   end
 
-  def self.reset_all
-    @@songs.clear
-  end
+  # def self.reset_all
+  #   @@songs.clear
+  # end
 
   def to_param
     name.downcase.gsub(' ', '-')
