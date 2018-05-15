0100629b54588b5b35529a6efc7627738ff10518
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-15 22:26:46 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,8 +2,10 @@ require 'pry'
 
 class Song
 
-  # extend memorable
-  include memorable
+  extend Memorable::ClassMethods
+  include Memorable::InstanceMethods
+  extend Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
