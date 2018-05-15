d87f93ee0d3f31adba711a1020772f9a563fb8cd
abdillahi Bashir <abdillahi.muse@hotmail.com>
2018-05-15 22:08:14 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -11,12 +11,8 @@ class Artist
   @@artists = []
 
   def initialize
-    song
+    songs
     @songs = []
-  end
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
   end
 
   def self.all
@@ -28,20 +24,8 @@ class Artist
     song.artist = self
   end
 
-  # def self.reset_all
-  #   self.all.clear
-  # end
-
-  # def self.count
-  #   @@artists.count
-  # end
-
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
-
-  # def to_param
-  #   name.downcase.gsub(' ', '-')
-  # end
 
 end
